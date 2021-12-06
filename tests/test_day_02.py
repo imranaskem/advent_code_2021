414b12acbf9f1f84a34de5b7ed35f5682dbeccdc
Imran Askem <imran.askem@gmail.com>
2021-12-06 14:57:09 +0000

    Part 2

--- tests/test_day_02.py
+++ tests/test_day_02.py
@@ -4,4 +4,4 @@ from utils.utils import load_text_file
 
 def test_calculate_distance():
     instructions = load_text_file('day02_input.txt', __file__, lambda s: s.split(' '))
-    assert calculate_distance(instructions) == 150
+    assert calculate_distance(instructions) == 900
