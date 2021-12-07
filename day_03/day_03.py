2c674d23d469b3a384dc7289c5e7aaafbe5f9e66
Imran Askem <imran.askem@gmail.com>
2021-12-07 20:42:46 +0000

    Day 3, part 2

--- day_03/day_03.py
+++ day_03/day_03.py
@@ -1,21 +1,28 @@
+import operator
+
 from utils.utils import load_text_file
 
 
-def calculate_rates(binaries: list) -> int:
-    threshold = len(binaries) // 2
+def calculate_rating(binaries: list, op) -> str:
     binary_length = len(binaries[0]) - 1
-    gamma, epsilon = '', ''
     for i in range(0, binary_length):
-        if sum([1 for bin in binaries if bin[i] == '1']) > threshold:
-            gamma += '1'
+        if len(binaries) == 1:
+            break
+        one_list = [bin for bin in binaries if bin[i] == '1']
+        zero_list = [bin for bin in binaries if bin[i] == '0']
+        if len(one_list) == len(zero_list):
+            binaries = one_list.copy() if op == operator.lt else zero_list.copy()
+        elif op(len(one_list), len(zero_list)):
+            binaries = zero_list.copy()
         else:
-            gamma += '0'
-    for char in gamma:
-        if char == '1':
-            epsilon += '0'
-        else:
-            epsilon += '1'
-    return int(gamma, 2) * int(epsilon, 2)
+            binaries = one_list.copy()
+    return binaries[0]
+
+
+def calculate_rates(binaries: list) -> int:
+    co_binary = calculate_rating(binaries.copy(), operator.gt)
+    oxygen_binary = calculate_rating(binaries.copy(), operator.lt)
+    return int(co_binary, 2) * int(oxygen_binary, 2)
 
 
 if __name__ == '__main__':
--- tests/test_day_03.py
+++ tests/test_day_03.py
@@ -4,4 +4,4 @@ from utils.utils import load_text_file
 
 def test_calculate_rates():
     binaries = load_text_file('day03_input.txt', __file__, lambda s: s)
-    assert calculate_rates(binaries) == 198
+    assert calculate_rates(binaries) == 230
